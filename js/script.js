a298ce6c9eb8351d9e3cf983366f21c58c578cb3
Naxxfall <fed0r492@yandex.ru>
2020-09-29 19:23:43 +0300

    Начал js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var slide1 = document.querySelector(".slider__slide1");
+var slide2 = document.querySelector(".slider__slide2");
+var slide3 = document.querySelector(".slider__slide3");
+var slideControl1 = document.querySelector(".slider__control1");
+var slideControl2 = document.querySelector(".slider__control2");
+var slideControl3 = document.querySelector(".slider__control2");
+
+slideControl2.addEventListener("click", function(evt){
+  
+});
