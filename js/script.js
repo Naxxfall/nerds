aa3e28c36f9f66b6a0b085f2eba1b91b584381b2
Serj <pedro8@list.ru>
2020-09-30 16:28:30 +0300

    js слайдера, стили главной, кнопок, карта

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
-var slide1 = document.querySelector(".slider__slide1");
-var slide2 = document.querySelector(".slider__slide2");
-var slide3 = document.querySelector(".slider__slide3");
-var slideControl1 = document.querySelector(".slider__control1");
-var slideControl2 = document.querySelector(".slider__control2");
-var slideControl3 = document.querySelector(".slider__control2");
+var slides = document.querySelectorAll(".slide-wrapper");
+var sliderControls = document.querySelectorAll(".slider__control");
+var controlIndicators = document.querySelectorAll(".slider__control-indicator");
+var showIfJS = document.querySelectorAll(".no-js-hide");
 
-slideControl2.addEventListener("click", function(evt){
-  
+showIfJS.forEach((element) =>{
+  element.classList.remove("no-js-hide");
 });
+
+function changeSlide(slides, number){
+  slides.forEach((slide)  => {
+    slide.classList.remove("slider__slide--show");
+  });
+  slides[number].classList.add("slider__slide--show");
+}
+
+function changeSliderControls(controls, indicators, number) {
+  controls.forEach((control, index) => {
+    control.disabled = false;
+    indicators[index].classList.remove("slider__control-indicator--current");
+  });
+  controls[number].disabled = true;
+  indicators[number].classList.add("slider__control-indicator--current");
+}
+
+sliderControls.forEach((slideControl, index) => {
+  slideControl.addEventListener("click", function(evt){
+    evt.preventDefault();
+    changeSlide(slides, index);
+    changeSliderControls(sliderControls, controlIndicators, index);
+  });
+});
+
+
