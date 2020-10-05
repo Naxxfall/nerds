63d84d5297f74633514c3a892bae7d2018b62a97
Serj <pedro8@list.ru>
2020-10-05 16:01:08 +0300

    Слайдер диапазона цен каталога

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-var slides = document.querySelectorAll(".slide-wrapper");
-var sliderControls = document.querySelectorAll(".slider__control");
-var controlIndicators = document.querySelectorAll(".slider__control-indicator");
-var showIfJS = document.querySelectorAll(".no-js-hide");
+const slides = document.querySelectorAll(".slide-wrapper");
+const sliderControls = document.querySelectorAll(".slider__control");
+const controlIndicators = document.querySelectorAll(".slider__control-indicator");
+const showIfJS = document.querySelectorAll(".no-js-hide");
+const rangeMin = document.querySelector(".range__scale--min");
+const rangeMax = document.querySelector(".range__scale--max");
+const priceMin = document.querySelector(".range-value__min-price");
+const priceMax = document.querySelector(".range-value__max-price");
+const numbers = /^[0-9]+$/;
 
 showIfJS.forEach((element) =>{
   element.classList.remove("no-js-hide");
@@ -31,4 +36,28 @@ sliderControls.forEach((slideControl, index) => {
   });
 });
 
+function checkScales(min, max){
+  if (min.value > max.value){
+    max.value=min.value;
+  }
+  if (max.value < min.value){
+    min.value=max.value;
+  }
+}
+
+function minRangeMove(){
+  if (+rangeMin.value > +rangeMax.value){
+    rangeMax.value = +rangeMin.value;
+    priceMax.value = rangeMax.value;
+  }
+  priceMin.value=rangeMin.value;
+}
+
+function maxRangeMove(){
+  if (+rangeMax.value < +rangeMin.value){
+    rangeMin.value = +rangeMax.value;
+    priceMin.value = rangeMin.value;
+  }
+  priceMax.value=rangeMax.value;
+}
 
